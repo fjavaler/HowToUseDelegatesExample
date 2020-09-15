bf3fcb22f67927f7bab8a9ee7e0b187400ef32b5
fjavaler <frederickjavalera@gmail.com>
2020-09-14 21:11:49 -0600

    Complete.

---
+++ HowToUseDelegatesExample/Employee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HowToUseDelegatesExample
+{
+  //delegate.
+  internal delegate bool IsPromotable(Employee empl);
+
+  /// <summary>
+  /// Employee class.
+  /// </summary>
+  internal class Employee
+  {
+    //Employee properties.
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public int Salary { get; set; }
+    public int Experience { get; set; }
+
+    /// <summary>
+    /// This method uses the IsPromotable delegate as a "stand in" for an implemented method.
+    /// Actual implementation of delegate method is done in Program.cs.
+    /// </summary>
+    /// <param name="employeeList">A list of employees.</param>
+    /// <param name="IsEligibleToPromote">delegate method used as "stand in."</param>
+    public static void PromoteEmployee(List<Employee> employeeList, IsPromotable IsEligibleToPromote)
+    {
+      foreach (Employee employee in employeeList)
+      {
+        if (IsEligibleToPromote(employee))
+        {
+          Console.WriteLine(employee.Name + " promoted.");
+        }
+      }
+    }
+  }
+}
--- HowToUseDelegatesExample/Program.cs
+++ HowToUseDelegatesExample/Program.cs
@@ -1,12 +1,48 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace HowToUseDelegatesExample
 {
-  class Program
+  public class Program
   {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
+      //create a list of employees
+      var empList = new List<Employee>
+      {
+        new Employee() { ID = 101, Name = "Mary", Salary = 50000, Experience = 5 },
+        new Employee() { ID = 102, Name = "Mike", Salary = 40000, Experience = 4 },
+        new Employee() { ID = 103, Name = "John", Salary = 60000, Experience = 6 },
+        new Employee() { ID = 104, Name = "Todd", Salary = 30000, Experience = 3 }
+      };
+
+      //declare and initialize the delegate
+      IsPromotable isPromotable = new IsPromotable(Promote);
+
+      //pass the list and delegate into the PromoEmployee method.
+      Console.WriteLine(">Using Promote method:");
+      Employee.PromoteEmployee(empList, isPromotable);
+
+      Console.WriteLine("\n>Using lambda expression:");
+      //alternately, you can use a lambda expression to replace line 24. If being used, Promote method below may be deleted.
+      Employee.PromoteEmployee(empList, emp => emp.Experience >= 5);
+    }
+
+    /// <summary>
+    /// The delegate method.
+    /// </summary>
+    /// <param name="empl">An employee.</param>
+    /// <returns>True or false.</returns>
+    private static bool Promote(Employee empl)
+    {
+      if (empl.Experience >= 5)
+      {
+        return true;
+      }
+      else
+      {
+        return false;
+      }
     }
   }
 }
